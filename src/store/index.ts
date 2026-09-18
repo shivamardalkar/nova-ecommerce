4aeaeba51afb604dbac42d53047cc2467032ce11
shivamardalkar <shivamardalkar@gmail.com>
2026-09-18 13:50:43 +0530

    feat: add product redux slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
+import productReducer from './slices/productSlice';
 import uiReducer from './slices/uiSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        products: productReducer,
         ui: uiReducer,
     },
 });
