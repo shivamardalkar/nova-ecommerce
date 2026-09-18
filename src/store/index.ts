68567a625327d6ec0660bb58d29f4dc28621aced
shivamardalkar <shivamardalkar@gmail.com>
2026-09-18 13:55:55 +0530

    feat: add category redux slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
+import categoryReducer from './slices/categorySlice';
 import productReducer from './slices/productSlice';
 import uiReducer from './slices/uiSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        categories: categoryReducer,
         products: productReducer,
         ui: uiReducer,
     },
